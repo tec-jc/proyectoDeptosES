649233d11a3ceafb5f076a2bc7f3664cf024f56b
JC Tula <jtula_esfe@outlook.es>
2023-09-11 11:19:51 -0600

    Programación de las clases BL

---
+++ DeptosES.LogicaDeNegocio/DepartamentoBL.cs
@@ -0,0 +1,43 @@
+﻿using DeptosES.AccesoADatos;
+using DeptosES.EntidadesDeNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeptosES.LogicaDeNegocio
+{
+    public class DepartamentoBL
+    {
+        public async Task<int> crearAsync(Departamento pDepartamento)
+        {
+            return await DepartamentoDAL.CrearAsync(pDepartamento);
+        }
+
+        public async Task<int> ModificarAsync(Departamento pDepartamento)
+        {
+            return await DepartamentoDAL.ModificarAsync(pDepartamento);
+        }
+
+        public async Task<int> EliminarAsync(Departamento pDepartamento)
+        {
+            return await DepartamentoDAL.EliminarAsync(pDepartamento);
+        }
+
+        public async Task<Departamento> ObtenerPorIdAsync(Departamento pDepartamento)
+        {
+            return await DepartamentoDAL.ObtenerPorIdAsync(pDepartamento);
+        }
+
+        public async Task<List<Departamento>> ObtenerTodosAsync()
+        {
+            return await DepartamentoDAL.ObtenerTodosAsync();
+        }
+
+        public async Task<List<Departamento>> BuscarAsync(Departamento pDepartamento)
+        {
+            return await DepartamentoDAL.BuscarAsync(pDepartamento);
+        }
+    }
+}
---
+++ DeptosES.LogicaDeNegocio/MunicipioBL.cs
@@ -0,0 +1,48 @@
+﻿using DeptosES.AccesoADatos;
+using DeptosES.EntidadesDeNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeptosES.LogicaDeNegocio
+{
+    public class MunicipioBL
+    {
+        public async Task<int> CrearAsync(Municipio pMunicipio)
+        {
+            return await MunicipioDAL.CrearAsync(pMunicipio);
+        }
+
+        public async Task<int> ModificarAsync(Municipio pMunicipio)
+        {
+            return await MunicipioDAL.ModificarAsync(pMunicipio);
+        }
+
+        public async Task<int> EliminarAsync(Municipio pMunicipio)
+        {
+            return await MunicipioDAL.EliminarAsync(pMunicipio);
+        }
+
+        public async Task<Municipio> ObtenerPorIdAsync(Municipio pMunicipio)
+        {
+            return await MunicipioDAL.ObtenerPorIdAsync(pMunicipio);
+        }
+
+        public async Task<List<Municipio>> ObtenerTodosAsync()
+        {
+            return await MunicipioDAL.ObtenerTodosAsync();
+        }
+
+        public async Task<List<Municipio>> BuscarAsync(Municipio pMunicipio)
+        {
+            return await MunicipioDAL.BuscarAsync(pMunicipio);
+        }
+
+        public async Task<List<Municipio>> BuscarIncluirDepartamentosAsync(Municipio pMunicipio)
+        {
+            return await MunicipioDAL.BuscarIncluirDepartamentosAsync(pMunicipio);
+        }
+    }
+}
